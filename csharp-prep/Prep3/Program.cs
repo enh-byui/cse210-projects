25850477ea7eae053aa3f609e7aed84a51e9753b
navaer <erick.nava@fastcode.today>
2023-01-12 11:00:40 -0700

    Creates assigments 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "y";
+
+        Console.WriteLine("Guess a number between 1 to 100");
+        
+
+        while (playAgain == "y") {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            //int magicNumber = -1;
+            int guessNumber = -1;
+            int counter = 1;
+            Console.WriteLine(magicNumber);
+
+            while (magicNumber != guessNumber) {
+
+                //Console.Write("What is the magic number? ");
+                //magicNumber = int.Parse(Console.ReadLine());
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guessNumber) {
+                    Console.WriteLine("Higher");
+                } else if (magicNumber < guessNumber) {
+                    Console.WriteLine("Lower");
+                } else {
+                    Console.WriteLine("Great! you did it.");
+                    Console.WriteLine($"It took {counter} times to guess the number.");
+
+                    Console.Write("Do you want to play again? (y/n) ");
+                    playAgain = Console.ReadLine();
+                }
+
+                counter += 1;
+            }
+
+            if (playAgain == "n") {
+                Console.WriteLine($"Thank you, Good bye!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int askNumber = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (askNumber != 0) {
+            Console.Write("Enter a number: ");
+            askNumber = int.Parse(Console.ReadLine());
+            
+
+            if (askNumber != 0) {
+                numbers.Add(askNumber);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers) {
+            sum += number;
+        }
+
+        float average = ((float)sum) / numbers.Count;
+
+        int maxValue = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maxValue)
+            {
+                maxValue = number;
+            }
+        }
+
+        //double average = numbers.Average();
+        //int sum = numbers.Sum();
+        //int maxValue = numbers.Max();
+
+
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The highest number is: {maxValue}");
+        
     }
 }
