1ac34999f8fa23538d20237593a2123b737fc10b
happy-arts <happy.arts.nft@gmail.com>
2023-02-20 17:37:11 -0600

    Developer04 Prove Activity First commit

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+
+        int featureSelection = 1;
+        Console.Clear();
+
+        while (featureSelection >= 1 && featureSelection < 4)
+        {
+
+            Console.WriteLine("Menu Options:");
+
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            featureSelection = int.Parse(Console.ReadLine());
+            
+            
+
+            if (featureSelection == 1)
+            {
+
+            }
+            else if (featureSelection == 4)
+            {
+                Console.WriteLine("See you tomorrow!");
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid option");
+            }
+
+        }
+
     }
 }
