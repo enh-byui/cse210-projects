93c24c1a0a606e43b9b0608a21f8144e35ad47a0
happy-arts <happy.arts.nft@gmail.com>
2023-04-01 10:15:03 -0600

    Creates Reception Event Class

---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,15 @@
+class Reception : Event
+{
+    private string _rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, string time, Address address, string rsvpEmail) : base(title, description, date, time, address)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails() {
+        return $"{GetStandardDetails()}\nRSVP Email: {_rsvpEmail}";
+    }
+
+
+}
