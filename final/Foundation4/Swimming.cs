7ce831aeb906f88e256699ff57e05e78ad9b1788
happy-arts <happy.arts.nft@gmail.com>
2023-04-01 11:13:36 -0600

    Foundation 4 First Commit

---
+++ final/Foundation4/Swimming.cs
@@ -0,0 +1,13 @@
+public class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+
+}
