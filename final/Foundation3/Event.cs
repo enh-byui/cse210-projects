51dbfcd76276aac3b96985d47205c33f58a81109
happy-arts <happy.arts.nft@gmail.com>
2023-04-01 09:41:38 -0600

    Foundation 3 First Commit

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -1,9 +1,36 @@
-class Event {
-    private string title;
-    private string description;
-    private DateTime date;
-    private string time;
-    //private Address address;
+class Event
+{
+    private string _title;
+    private string _description;
+    private DateTime _date;
+    private string _time;
+    private Address _address;
 
-    
+    public Event(string title, string description, DateTime date, string time, Address address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+
+    public string GetStandardDetails()
+    {
+        return $"Title: {_title}\nDescription: {_description}\nDate: {_date.ToShortDateString()}\nTime: {_time}\nAddress: {_address.GetAddress()}";
+    }
+
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();
+    }
+
+    public virtual string GetShortDescription()
+    {
+        return $"{GetType().Name}: {_title} - {_date.ToShortDateString()}";
+    }
+
+
+
+
 }
