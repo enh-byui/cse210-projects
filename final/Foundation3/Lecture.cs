5d57d6c94956494f6e95e4695c8d7bb19b36aa64
happy-arts <happy.arts.nft@gmail.com>
2023-04-01 10:11:10 -0600

    Creates Lecture Event

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -1,9 +1,9 @@
-class Lecture : Event 
+class Lecture : Event
 {
     private string _speaker;
     private int _capacity;
 
-    public Lecture(string title, string desctiption, string speaker, int capacity)
+    public Lecture(string title, string description, DateTime date, string time, Address address, string speaker, int capacity) : base(title, description, date, time, address)
     {
         _speaker = speaker;
         _capacity = capacity;
